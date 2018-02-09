800180725095ce3e0dfebc3481f9728b5d0ba4ff
Raul Kuzner <raul@kuzner.net>
2018-02-09 04:48:42 -0300

    use Map to retain and reuse values

--- src/main/java/net/kuzner/app/climaplanetas/RestServiceController.java
+++ src/main/java/net/kuzner/app/climaplanetas/RestServiceController.java
@@ -1,6 +1,7 @@
 package net.kuzner.app.climaplanetas;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,27 +19,38 @@ public class RestServiceController {
 	@Autowired
 	private Meteorologo meteorologo;
 
-	Collection<Pronostico> pronosticoCollection = null;
+	Map<Integer, Pronostico> pronosticoPorDia = null;
+	Prediccion ultimaPrediccion = null;
 
 	@RequestMapping(path = "/prediccion", method = RequestMethod.GET)
 	public Prediccion getPrediccion(@RequestParam(value = "dias", defaultValue = "3652") Integer cuantosDias) {
-		if (Objects.isNull(this.pronosticoCollection) || this.pronosticoCollection.isEmpty()) {
-			this.pronosticoCollection = meteorologo.obtenerPronosticos(cuantosDias, sistemaSolar.getPlanetaA(),
-					sistemaSolar.getPlanetaB(), sistemaSolar.getPlanetaC());
+		if (Objects.isNull(this.pronosticoPorDia) || this.pronosticoPorDia.isEmpty()) {
+			Collection<Pronostico> pronosticoCollection = meteorologo.obtenerPronosticos(cuantosDias,
+					sistemaSolar.getPlanetaA(), sistemaSolar.getPlanetaB(), sistemaSolar.getPlanetaC());
+			for (Pronostico pronostico : pronosticoCollection) {
+				this.pronosticoPorDia.put(pronostico.getDia(), pronostico);
+			}
+			this.ultimaPrediccion = this.meteorologo.predecirClima(this.pronosticoPorDia.values());
 		}
-		return this.meteorologo.predecirClima(this.pronosticoCollection);
+		return this.ultimaPrediccion;
 	}
 
 	@RequestMapping(path = "/prediccion", method = RequestMethod.DELETE)
 	public void deletePrediccion() {
-		if (Objects.nonNull(this.pronosticoCollection) && !this.pronosticoCollection.isEmpty()) {
-			this.pronosticoCollection.clear();
+		if (Objects.nonNull(this.pronosticoPorDia) && !this.pronosticoPorDia.isEmpty()) {
+			this.pronosticoPorDia.clear();
 		}
 	}
 
 	@RequestMapping(path = "/clima", method = RequestMethod.GET)
 	public Pronostico getPronostico(@RequestParam(value = "dia", defaultValue = "0") Integer queDia) {
-		return this.meteorologo.calcularClima(queDia, this.sistemaSolar.getPlanetaA(), this.sistemaSolar.getPlanetaB(),
-				this.sistemaSolar.getPlanetaC());
+		Pronostico pronosticoDeseado = null;
+		if (Objects.nonNull(this.pronosticoPorDia) && this.pronosticoPorDia.containsKey(queDia)) {
+			pronosticoDeseado = this.pronosticoPorDia.get(queDia);
+		} else {
+			pronosticoDeseado = this.meteorologo.calcularClima(queDia, this.sistemaSolar.getPlanetaA(),
+					this.sistemaSolar.getPlanetaB(), this.sistemaSolar.getPlanetaC());
+		}
+		return pronosticoDeseado;
 	}
 }
